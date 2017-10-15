2176751372282a52f5265d72debdb75e86a05e7e
Yvonne <princess0115@gmail.com>
2017-10-14 23:04:56 -0400

    connected all the screens

--- app/src/main/java/com/flyaway/app/FlightList.java
+++ app/src/main/java/com/flyaway/app/FlightList.java
@@ -1,14 +1,27 @@
 package com.flyaway.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FlightList extends AppCompatActivity {
 
+    Button pickBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_list);
+
+        pickBtn = (Button)findViewById(R.id.button3);
+        pickBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PaymentSummary.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/flyaway/app/PaymentSummary.java
+++ app/src/main/java/com/flyaway/app/PaymentSummary.java
@@ -1,13 +1,26 @@
 package com.flyaway.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PaymentSummary extends AppCompatActivity {
 
+    Button addCardBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_summary);
+
+        addCardBtn = (Button)findViewById(R.id.button2);
+        addCardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PaymentSchedule.class);
+                startActivity(intent);
+            }
+        });
     }
 }
