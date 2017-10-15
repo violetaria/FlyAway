dfcf11cdf56254941bd8ef48308447ab5b7e1868
violetaria <terri.chu@outlook.com>
2017-10-15 11:18:59 -0400

    update payment schedule with data from previous screen

--- app/src/main/java/com/flyaway/app/PaymentSummary.java
+++ app/src/main/java/com/flyaway/app/PaymentSummary.java
@@ -12,6 +12,11 @@ import java.util.Locale;
 import java.text.NumberFormat;
 
 public class PaymentSummary extends AppCompatActivity {
+    final static String WEEKLY_PAYMEMNT = "WEEKLY_PAYMENT";
+    final static String TODAYS_PAYMENT = "TODAYS_PAYMENT";
+    final static String TOTAL_PAYMENT = "TOTAL_PAYMENT";
+
+
     final static Integer MAX_SEEK_VALUE = 100;
     final static Double MIN_SERVICE_FEE = 200.00;
     Button addCardBtn;
@@ -20,6 +25,8 @@ public class PaymentSummary extends AppCompatActivity {
     TextView tvWeeklyPayments;
     TextView tvViewPaymentSchedule;
     Double totalPayment;
+    Double payTodayValue;
+    Double weeklyPayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +34,18 @@ public class PaymentSummary extends AppCompatActivity {
         setContentView(R.layout.activity_payment_summary);
 
         // TODO - get total payment from calling activity
-        totalPayment = 1200.00;
-
-        tvViewPaymentSchedule = (TextView)findViewById(R.id.tvViewPaymentSchedule);
+        totalPayment = 1250.00;
+        payTodayValue = 200.00;
+        weeklyPayment = (totalPayment - payTodayValue) / 6;
+        
+        tvViewPaymentSchedule = (TextView)findViewById(R.id.tvPayment3);
         tvViewPaymentSchedule.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), PaymentSchedule.class);
+                intent.putExtra(WEEKLY_PAYMEMNT, weeklyPayment);
+                intent.putExtra(TODAYS_PAYMENT, payTodayValue);
+                intent.putExtra(TOTAL_PAYMENT, totalPayment);
                 startActivity(intent);
             }
         });
@@ -47,7 +59,7 @@ public class PaymentSummary extends AppCompatActivity {
             }
         });
 
-        tvDueToday = (TextView)findViewById(R.id.tvDueToday);
+        tvDueToday = (TextView)findViewById(R.id.tvDate5);
         tvWeeklyPayments = (TextView)findViewById(R.id.tvWeeklyPayments);
         seekBar = (SeekBar)findViewById(R.id.seekBar);
         seekBar.setMax(MAX_SEEK_VALUE);
@@ -64,10 +76,10 @@ public class PaymentSummary extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 NumberFormat format = NumberFormat.getCurrencyInstance(Locale.getDefault());
-                Double payTodayValue = MIN_SERVICE_FEE + (progress * 300.00) / 100.00;
+                payTodayValue = MIN_SERVICE_FEE + (progress * 300.00) / 100.00;
                 tvDueToday.setText(format.format(payTodayValue) + " Due Today");
 
-                Double weeklyPayment = (totalPayment - payTodayValue) / 6;
+                weeklyPayment = (totalPayment - payTodayValue) / 6;
                 tvWeeklyPayments.setText(format.format(weeklyPayment) + " Every Other Week");
 
             }
