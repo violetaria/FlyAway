3ee4ca6051e9a3bb48f264b98e5f532c9c520616
violetaria <terri.chu@outlook.com>
2017-10-15 10:11:44 -0400

    seek bar change updates due today text view

--- app/src/main/java/com/flyaway/app/PaymentSummary.java
+++ app/src/main/java/com/flyaway/app/PaymentSummary.java
@@ -1,14 +1,22 @@
 package com.flyaway.app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Locale;
+import java.text.NumberFormat;
 
 public class PaymentSummary extends AppCompatActivity {
-
+    final static Integer MAX_SEEK_VALUE = 100;
     Button addCardBtn;
+    SeekBar seekBar;
+    TextView tvDueToday;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,27 @@ public class PaymentSummary extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), PaymentSchedule.class);
                 startActivity(intent);
+            }
+        });
+
+        tvDueToday = (TextView)findViewById(R.id.tvDueToday);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setMax(MAX_SEEK_VALUE);
+        seekBar.setProgress(0);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                NumberFormat format = NumberFormat.getCurrencyInstance(Locale.getDefault());
+                Double payTodayValue = 200.00 + (progress * 300.00) / 100.00;
+                tvDueToday.setText(format.format(payTodayValue) + " Due Today");
             }
         });
     }
